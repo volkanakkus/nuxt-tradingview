4e6c96e9b109043b8188de90b0246ac466ba8d76
Volkan Akkuş <volkanakks.dev@gmail.com>
2024-03-16 01:20:52 +0100

    docs: Playground Added

---
+++ .stackblitz/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Chart />
+    <CompanyProfile />
+    <CryptoHeatMap />
+    <CryptoMarket />
+    <EconomicCalendar />
+    <ForexCrossRates />
+    <ForexHeatMap />
+    <FundamentalData />
+    <MarketData />
+    <MarketOverview />
+    <MiniChart />
+    <Screener />
+    <SingleTicker />
+    <Snaps />
+    <StockMarket />
+    <SymbolInfo />
+    <SymbolOverview />
+    <TechnicalAnalysis />
+    <Ticker />
+    <TickerTape />
+  </div>
+</template>
---
+++ .stackblitz/nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: ['nuxt-tradingview'],
+
+  tradingview: {
+    // prefix: 'TV',
+    // importOnly: ['Chart'],
+  },
+
+  // This playground is still work in progress.
+
+  // Following configuration is only necessary to make Stackblitz work correctly.
+  // For local projects, you do not need any configuration to try it out.
+});
